03bbaee35cb9282d34dc2e5d938124d39aca39d9
Crazy502 <llqweb@163.com>
2025-11-06 18:42:12 +0800

    feat(user): 实
    -现用户注册功能 添加密码哈希处理函数
    - 实现邮箱唯一性检查
    - 添加用户创建时间和更新时间字段
    - 使用 MongoDB 存储用户数据
    - 返回创建成功的用户信息

--- Server/MagicStreamMoviesServer/controllers/user_controller.go
+++ Server/MagicStreamMoviesServer/controllers/user_controller.go
@@ -1,12 +1,29 @@
 package controllers
 
 import (
+	"context"
 	"net/http"
+	"time"
 
+	"github.com/crazy502/MagicStreamMovies/Server/MagicStreamMoviesServer/database"
 	"github.com/crazy502/MagicStreamMovies/Server/MagicStreamMoviesServer/models"
 	"github.com/gin-gonic/gin"
 	"github.com/go-playground/validator/v10"
+	"go.mongodb.org/mongo-driver/v2/bson"
+	"go.mongodb.org/mongo-driver/v2/mongo"
+	"golang.org/x/crypto/bcrypt"
 )
+
+var userCollection *mongo.Collection = database.OpenCollection("users")
+
+func HashPassword(password string) (string, error) {
+	HashPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return "", err
+	}
+
+	return string(HashPassword), nil
+}
 
 func RegisterUser() gin.HandlerFunc {
 	return func(c *gin.Context) {
@@ -22,5 +39,40 @@ func RegisterUser() gin.HandlerFunc {
 			c.JSON(http.StatusBadRequest, gin.H{"error": "Validation failed", "details": err.Error()})
 			return
 		}
+
+		hashedPassword, err := HashPassword(user.Password)
+
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Ubable to hash password"})
+			return
+		}
+
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+		defer cancel()
+
+		count, err := userCollection.CountDocuments(ctx, bson.M{"email": user.Email})
+
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to check existing user"})
+			return
+		}
+		if count > 0 {
+			c.JSON(http.StatusConflict, gin.H{"error": "User already exists"})
+			return
+		}
+		user.UserID = bson.NewObjectID().Hex()
+		user.CreatedAt = time.Now()
+		user.UpdatedAt = time.Now()
+		user.Password = hashedPassword
+
+		result, err := userCollection.InsertOne(ctx, user)
+
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to create user"})
+			return
+		}
+
+		c.JSON(http.StatusCreated, result)
+
 	}
 }
