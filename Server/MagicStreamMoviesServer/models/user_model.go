99477af9b3e161e72bb3581e50b7c08e6d0e7607
Crazy502 <llqweb@163.com>
2025-11-07 15:53:02 +0800

    feat(user): 添加用户登录和响应结构体
    - 新增 UserLogin 结构体用于处理用户登录信息- 新增 UserResponse 结构体作为用户数据传输对象- UserResponse 包含用户基本信息和偏好类型
    - 添加了邮箱和密码的验证标签
    - 为密码字段设置了最小长度验证规则
    - 定义了安全的数据传输模型，避免暴露敏感信息

--- Server/MagicStreamMoviesServer/models/user_model.go
+++ Server/MagicStreamMoviesServer/models/user_model.go
@@ -20,3 +20,23 @@ type User struct {
 	RefreshToken    string        `json:"refresh_token" bson:"refresh_token"`
 	FavouriteGenres []Genre       `json:"favourite_genres" bson:"favourite_genres" validate:"required,dive"`
 }
+
+type UserLogin struct {
+	Email    string `json:"email" validate:"email,required"`
+	Password string `json:"password" validate:"required,min=6"`
+}
+
+// 创建用户响应结构体，它将成为一个子集
+// DTO模型，它代表数据传输对象，是一种用于在软件应用层之间传输数据的设计模式，仅用于保存数据
+// 通常包含字段，属性，获取器和设置器，不包含业务逻辑
+// 可序列化，设计用于轻松序列化为JSO你，XML等
+// 使用场景：不暴露内部领域模型的情况下，向客户端返回数据的API响应
+// 通过使用DTO，仅暴露需要暴露给客户端的数据
+type UserResponse struct {
+	UserId          string  `json:"user_id"`
+	FirstName       string  `json:"first_name"`
+	LastName        string  `json:"last_name"`
+	Email           string  `json:"email"`
+	Role            string  `json:"role"`
+	FavouriteGenres []Genre `json:"favourite_genres"`
+}
