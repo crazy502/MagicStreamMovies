084db28a8e4ff915c6771d15166a0a3cc2a4832a
Crazy502 <llqweb@163.com>
2025-11-10 15:01:15 +0800

    feat(movie): 添加管理员评论更新功能- 新增 AdminReviewUpdate 控制器函数，用于更新电影- 实现 GetReviewRanking的管理员评论 函数，通过 AI 模型分析评论情感并返回评分 - 集成阿里云 Qwen 模型进行评论情感分析 - 新增 GetRankings 函数，从数据库获取评分标准 - 添加 rankingCollection 用于存储评分数据 - 引入 godotenv 支持环境变量加载 - 增加对 .env 文件缺失的警告日志 - 优化错误处理逻辑，确保及时返回 HTTP 错误响应

--- Server/MagicStreamMoviesServer/controllers/movie_controller.go
+++ Server/MagicStreamMoviesServer/controllers/movie_controller.go
@@ -2,19 +2,27 @@ package controllers
 
 import (
 	"context"
+	"errors"
+	"log"
 	"net/http"
+	"os"
+	"strings"
 	"time"
+
+	"github.com/go-playground/validator/v10"
+	"github.com/joho/godotenv"
+	"github.com/tmc/langchaingo/llms/openai"
+	"go.mongodb.org/mongo-driver/v2/bson"
+	"go.mongodb.org/mongo-driver/v2/mongo"
 
 	"github.com/crazy502/MagicStreamMovies/Server/MagicStreamMoviesServer/database"
 	"github.com/crazy502/MagicStreamMovies/Server/MagicStreamMoviesServer/models"
-	"github.com/go-playground/validator/v10"
-	"go.mongodb.org/mongo-driver/v2/bson"
-	"go.mongodb.org/mongo-driver/v2/mongo"
 
 	"github.com/gin-gonic/gin"
 )
 
 var movieCollection *mongo.Collection = database.OpenCollection("movies")
+var rankingCollection *mongo.Collection = database.OpenCollection("rankings")
 
 var validate = validator.New()
 
@@ -34,19 +42,16 @@ func GetMovies() gin.HandlerFunc {
 		cursor, err := movieCollection.Find(ctx, bson.M{})
 
 		if err != nil {
-			// log.Printf("Failed to fetch movies: %v", err)
 			c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to fetch movies: " + err.Error()})
-			// return
+			return
 		}
 		defer cursor.Close(ctx)
 
 		if err = cursor.All(ctx, &movies); err != nil {
-			// log.Printf("Failed to decode movies: %v", err)
 			c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to decode movies: " + err.Error()})
-			// return
+			return
 		}
 
-		// log.Printf("Successfully fetched %d movies", len(movies))
 		c.JSON(http.StatusOK, movies)
 	}
 }
@@ -106,4 +111,141 @@ func AddMovie() gin.HandlerFunc {
 
 		c.JSON(http.StatusCreated, result)
 	}
+}
+
+func AdminReviewUpdate() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		movieId := c.Param("imdb_id")
+		if movieId == "" {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "Movie Id is required"})
+			return
+		}
+		var req struct {
+			AdminReview string `json:"admin_review"`
+		}
+		var resp struct {
+			RankingName string `json:"ranking_name"`
+			AdminReview string `json:"admin_review"`
+		}
+
+		if err := c.ShouldBind(&req); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid request body"})
+			return
+		}
+		sentiment, rankVal, err := GetReviewRanking(req.AdminReview)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Error getting review ranking"})
+			return
+		}
+
+		filter := bson.M{"imdb_id": movieId}
+
+		update := bson.M{
+			"$set": bson.M{
+				"admin_review": req.AdminReview,
+				"ranking": bson.M{
+					"ranking_value": rankVal,
+					"ranking_name":  sentiment,
+				},
+			},
+		}
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+		defer cancel()
+		result, err := movieCollection.UpdateOne(ctx, filter, update)
+
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Error updating movie"})
+			return
+		}
+
+		if result.MatchedCount == 0 {
+			c.JSON(http.StatusNotFound, gin.H{"error": "Movie not found"})
+			return
+		}
+
+		resp.RankingName = sentiment
+		resp.AdminReview = req.AdminReview
+
+		c.JSON(http.StatusOK, resp)
+
+	}
+
+}
+
+func GetReviewRanking(admin_review string) (string, int, error) {
+	rankings, err := GetRankings()
+
+	if err != nil {
+		return "", 0, err
+	}
+
+	sentimentDelimited := ""
+
+	for _, ranking := range rankings {
+		if ranking.RankingValue != 999 {
+			sentimentDelimited = sentimentDelimited + ranking.RankingName + ","
+		}
+	}
+	sentimentDelimited = strings.Trim(sentimentDelimited, ",")
+
+	//仅在为测试和开发目的在本地运行时有效
+	err = godotenv.Load(".env")
+
+	if err != nil {
+		log.Println("Warning: .env file not found")
+	}
+
+	QwenApiKey := os.Getenv("QWEN_FLASH_API_KEY")
+	if QwenApiKey == "" {
+		return "", 0, errors.New("could not read QWEN_FLASH_API_KEY")
+	}
+
+	llm, err := openai.New(
+		openai.WithToken(QwenApiKey),
+		openai.WithBaseURL("https://dashscope.aliyuncs.com/compatible-mode/v1/"),
+	)
+
+	if err != nil {
+		return "", 0, err
+	}
+
+	base_prompt_template := os.Getenv("BASE_PROMPT_TEMPLATE")
+
+	base_promt := strings.Replace(base_prompt_template, "{rankings}", sentimentDelimited, 1)
+
+	response, err := llm.Call(context.Background(), base_promt+admin_review)
+
+	if err != nil {
+		return "", 0, err
+	}
+	rankVal := 0
+
+	for _, ranking := range rankings {
+		if ranking.RankingName == response {
+			rankVal = ranking.RankingValue
+			break
+		}
+	}
+	return response, rankVal, nil
+}
+
+func GetRankings() ([]models.Ranking, error) {
+	var rankings []models.Ranking
+
+	var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+	defer cancel()
+
+	cursor, err := rankingCollection.Find(ctx, bson.M{})
+
+	if err != nil {
+		return nil, err
+	}
+	defer cursor.Close(ctx)
+
+	if err := cursor.All(ctx, &rankings); err != nil {
+		return nil, err
+	}
+
+	return rankings, nil
+
 }
