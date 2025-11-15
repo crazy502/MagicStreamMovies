f10ba55174e968ff0f54cf40b75a7f09a8deb8ee
Crazy502 <llqweb@163.com>
2025-11-15 14:35:17 +0800

    refactor(routes): 更新受保护路由以支持 MongoDB 客户端依赖注入
    - 修改 SetupProtectedRoutes 函数签名以接收 MongoDB 客户端实例
    - 更新所有控制器调用以传递 MongoDB 客户端- 为受保护路由添加 MongoDB 驱动程序导入声明

--- Server/MagicStreamMoviesServer/routes/protected_routes.go
+++ Server/MagicStreamMoviesServer/routes/protected_routes.go
@@ -2,17 +2,18 @@ package routes
 
 import (
 	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/v2/mongo"
 
 	controller "github.com/crazy502/MagicStreamMovies/Server/MagicStreamMoviesServer/controllers"
 	"github.com/crazy502/MagicStreamMovies/Server/MagicStreamMoviesServer/middleware"
 )
 
-func SetupProtectedRoutes(router *gin.Engine) {
+func SetupProtectedRoutes(router *gin.Engine, client *mongo.Client) {
 	router.Use(middleware.AuthMiddleware())
 
-	router.GET("/movie/:imdb_id", controller.GetMovie())
-	router.POST("/addmovie", controller.AddMovie())
-	router.GET("/recommendedmovies", controller.GetRecommendedMovies())
-	router.PATCH("/updatereview/:imdb_id", controller.AdminReviewUpdate())
+	router.GET("/movie/:imdb_id", controller.GetMovie(client))
+	router.POST("/addmovie", controller.AddMovie(client))
+	router.GET("/recommendedmovies", controller.GetRecommendedMovies(client))
+	router.PATCH("/updatereview/:imdb_id", controller.AdminReviewUpdate(client))
 
 }
